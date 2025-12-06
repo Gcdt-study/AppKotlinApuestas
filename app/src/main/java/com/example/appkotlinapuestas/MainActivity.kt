54a97afec303024d2a5726410fc8ff66ea112989
Gcdt-study <gabriel110502004@institutodh.net>
2025-12-06 20:26:03 +0100

    nav básica añadida

--- app/src/main/java/com/example/appkotlinapuestas/MainActivity.kt
+++ app/src/main/java/com/example/appkotlinapuestas/MainActivity.kt
@@ -4,26 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.appkotlinapuestas.ui.theme.AppKotlinApuestasTheme
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            AppKotlinApuestasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContent { // Aquí defines toda la interfaz de usuario con Jetpack Compose.
+            MaterialTheme {
+                val navController = rememberNavController() // Crea y recuerda un NavController, que es el objeto que gestiona la navegación entre pantallas
+                NavHost( // Define el contenedor de navegacion
+                    navController = navController,
+                    startDestination = "principal" // indica la primera pantalla
+                ) {
+                    composable("principal") { PantallaPrincipal(navController) } // crea pantalla 1
+                    composable("segunda") { PantallaSecundaria(navController) } // crea pantalla 2
                 }
             }
         }
@@ -31,17 +38,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PantallaPrincipal(navController: NavController) { //recibe el NavController, el objeto que gestiona la navegación entre pantallas.
+
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) { // Organiza los elementos en una columna vertical.
+        Text("Pantalla Principal")
+        Button(onClick = { navController.navigate("segunda") }) { // Crea un botón, al pulsarlo, se ejecuta la navegación hacia la ruta "segunda", que corresponde a la pantalla secundaria definida en tu NavHost
+
+            Text("Ir a segunda pantalla")
+        }
+    }
+}
+
+@Composable
+fun PantallaSecundaria(navController: NavController) { //recibe el NavController, el objeto que gestiona la navegación entre pantallas.
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        Text("Pantalla Secundaria")
+        Button(onClick = { navController.popBackStack() }) { // Crea un botón, al pulsarlo, se ejecuta popBackStack(), que retrocede a la pantalla anterior en la pila de navegación.
+
+            Text("Volver")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AppKotlinApuestasTheme {
-        Greeting("Android")
-    }
+
 }
